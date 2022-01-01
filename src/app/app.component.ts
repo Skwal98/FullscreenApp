ab44391abe7709264f9789bf92fdff0f8657bfde
Skwal98 <levkalex98@gmail.com>
2022-01-01 22:33:31 +0300

    full screen app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import {
+  ChangeDetectorRef,
+  Component,
+  HostListener,
+  Inject,
+  OnInit,
+} from '@angular/core';
+import { fromEvent, merge } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'FullscreenApp';
+export class AppComponent implements OnInit {
+  public get isFullScreen(): boolean {
+    return window.innerHeight == screen.height;
+  }
+
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private _cd: ChangeDetectorRef
+  ) {}
+  ngOnInit(): void {
+    fromEvent(window, 'resize').subscribe(() => {
+      this._cd.detectChanges();
+    });
+  }
+
+  openFullScreen() {
+    this.document.documentElement.requestFullscreen();
+  }
 }
